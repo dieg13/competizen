ec218faf00e71c04866ccd7513442bcefe3b2f73
da-admin <da.admin@domtest.test>
2025-07-20 16:30:31 +0200

    fix: indentation

--- apps/backend/tests/Controller/TestControllerTest.php
+++ apps/backend/tests/Controller/TestControllerTest.php
@@ -1,19 +1,19 @@
 <?php
 
- namespace App\Tests\Controller;
+namespace App\Tests\Controller;
 
- use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
- uses(WebTestCase::class)->group('Functional');
+uses(WebTestCase::class)->group('Functional');
 
- test('La route de test fonctionne correctement', function () {
-     $client = static::createClient();
-     $client->request('GET', '/test');
+test('La route de test fonctionne correctement', function () {
+    $client = static::createClient();
+    $client->request('GET', '/test');
 
-     expect($client->getResponse()->isSuccessful())
-         ->toBeTrue()
-         ->and($client->getResponse()->getStatusCode())
-         ->toBe(200)
-         ->and($client->getResponse()->getContent())
-         ->toContain('Installation Symfony fonctionnelle !');
- });
+    expect($client->getResponse()->isSuccessful())
+        ->toBeTrue()
+        ->and($client->getResponse()->getStatusCode())
+        ->toBe(200)
+        ->and($client->getResponse()->getContent())
+        ->toContain('Installation Symfony fonctionnelle !');
+});
